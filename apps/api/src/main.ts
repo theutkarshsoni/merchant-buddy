df65cbf68ba54790bcb814da4ab39b10c75658a8
Utkarsh Soni <utkarshsoni12@gmail.com>
2025-10-08 12:56:39 +0100

    fix: Add CORS on the API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,12 @@ async function bootstrap() {
     AppModule, 
     new FastifyAdapter()
   );
+  await app.register(fastifyCors, {
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['content-type', 'authorization'],
+    credentials: true
+  });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
